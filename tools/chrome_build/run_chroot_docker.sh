9246907147dd061b2d474924b4bf319ce1d28d68
Gal Tashma <galtashma@gmail.com>
2019-06-01 18:00:46 +0800

    removed unused mount from run_chroot_docker

--- tools/chrome_build/run_chroot_docker.sh
+++ tools/chrome_build/run_chroot_docker.sh
@@ -2,7 +2,6 @@ ROOT=$HOME/dev/os-universe-new
 docker run  --mount type=bind,source=$ROOT/third_party/cros-sdk,target=/root/chroot \
             --mount type=bind,source=$ROOT/third_party/chromium/,target=/root/chroot/chromium \
             --mount type=bind,source=$ROOT/userspace/,target=/root/chroot/userspace \
-            --mount type=bind,source=$ROOT/out/chrome.img,target=/root/chroot/chrome.img \
     --cap-add=SYS_ADMIN \
     --privileged \
     -it osu-docker-chroot -l
